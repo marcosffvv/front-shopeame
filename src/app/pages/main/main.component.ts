b105f1b520ce6e80cdf6bad1a0a1ae07601df0e1
marcosffvv <marcosfrailefdz02@gmail.com>
2023-01-24 15:05:12 +0100

    prueba

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -16,13 +16,13 @@ export class MainComponent implements OnInit {
   ngOnInit(): void {
       this.llamadaService.getProducts().subscribe((data: any) => {
         //console.log(data);
-        this.productsList = data
-        this.paintedProducts = data
+        this.productsList = [...data]
+        this.paintedProducts = [...data]
       })
   }
 
   filtrar(nombre: string){
     let filteredProduct = this.productsList.filter((product) => product.name.includes(nombre))
-    this.paintedProducts = filteredProduct;
+    this.paintedProducts = [...filteredProduct];
   }
 }
